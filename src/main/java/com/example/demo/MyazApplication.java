038a7bcbdb62f1f30327eebe7d695e1bd0ec4ec3
timo <timo.guenzel@evia.de>
2021-08-26 12:18:11 +0200

    test

--- src/main/java/com/example/demo/MyazApplication.java
+++ src/main/java/com/example/demo/MyazApplication.java
@@ -1,40 +1,41 @@
 package com.example.demo;
 
 import com.example.demo.model.Person;
-import com.example.demo.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class MyazApplication {
 
-    private final PersonService personService;
+//    private final PersonService personService;
 
     public static void main(String[] args) {
-        SpringApplication.run(MyazApplication.class, args);
+        SpringApplication.run(com.example.demo.MyazApplication.class, args);
     }
 
-    @Autowired
-    public MyazApplication(PersonService personService) {
-        this.personService = personService;
-    }
-
-    @PostMapping("/people")
-    public void addPerson(@RequestBody Person person) {
-        personService.addPerson(person);
-    }
+//    @Autowired
+//    public MyazApplication(PersonService personService) {
+//        this.personService = personService;
+//    }
+//
+//    @PostMapping("/people")
+//    public void addPerson(@RequestBody Person person) {
+//        personService.addPerson(person);
+//    }
 
     @GetMapping("/peoples")
     public List<Person> getAllPeople() {
-        return personService.getAllPeople();
+        Person p = new Person(1, "ttttest");
+        List<Person> list = new ArrayList<Person>();
+        list.add(p);
+        return list;
+ //       return personService.getAllPeople();
     }
 
 }
